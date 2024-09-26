d0e98fbcbef43c1c1c660f701832bddae0b50885
Sudhir Yadav <sudhir9297@gmail.com>
2024-09-26 15:06:54 +0530

    Github Link in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import Logo from "./Logo";
-import { XLogo } from "@/data/svg";
+import { Github, XLogo } from "@/data/svg";
 
 const Header = () => {
   return (
     <nav className="text-lightGreen flex w-full items-center justify-between border-b py-4">
       <Logo />
-      <a
-        href="https://x.com/SeeOn__"
-        target="_blank"
-        rel="noreferrer"
-        className="h-8 w-8 cursor-pointer p-1.5"
-      >
-        <XLogo />
-      </a>
+      <div className="flex items-center gap-2">
+        <a
+          href="https://x.com/SeeOn__"
+          target="_blank"
+          rel="noreferrer"
+          className="h-8 w-8 cursor-pointer p-1.5"
+        >
+          <XLogo />
+        </a>
+
+        <a
+          href="https://github.com/sudhir9297/clover-product-page-r3f"
+          target="_blank"
+          rel="noreferrer"
+          className="h-6 w-6 cursor-pointer text-black"
+        >
+          <Github />
+        </a>
+      </div>
     </nav>
   );
 };
